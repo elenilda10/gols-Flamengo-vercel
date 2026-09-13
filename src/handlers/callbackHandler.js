9e3a99b2094bbb7b86c318532a102a846b6838fe
elenilda10 <126091743+elenilda10@users.noreply.github.com>
2026-09-13 08:48:19 -0300

    Route /ganhou callbacks through dedicated handler

--- src/handlers/callbackHandler.js
+++ src/handlers/callbackHandler.js
@@ -1,7 +1,13 @@
 import { encaminharParaMotorAtual } from "./legacyAdapter.js";
+import { processarCallbackGanhou } from "../services/winnerAdmin.js";
 
 export async function handleCallback(update, env) {
     if (!update?.callback_query) {
+        return new Response("OK", { status: 200 });
+    }
+
+    const processadoComoGanhou = await processarCallbackGanhou(update, env);
+    if (processadoComoGanhou) {
         return new Response("OK", { status: 200 });
     }
 
